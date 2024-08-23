38b1b0ade0819b214e92de7f491df8405c24fc85
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-08-23 18:52:30 +0300

    add : components into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+
 export default function App() {
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <Footer />
     </>
   )
 }
